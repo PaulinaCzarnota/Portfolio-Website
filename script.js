862bbe19536c00e62ff6ada284c67a8c3ad6aa0b
Paulina Czarnota <paul.czar.14@gmail.com>
2024-10-13 01:41:36 +0100

    new

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
+  
+  if (menu && icon) {  // Check if elements exist
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
 }
