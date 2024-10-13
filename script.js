19be7ee0c3806dc036e518ad54370215b244e302
Paulina Czarnota <paul.czar.14@gmail.com>
2024-10-13 01:32:09 +0100

    new

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
-
-  // Toggle the open class to show/hide the menu
   menu.classList.toggle("open");
-  
-  // Toggle the open class on the icon to animate the hamburger
   icon.classList.toggle("open");
 }
